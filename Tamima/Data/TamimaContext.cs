1ea8ab5b5722cb6403b2cdb06341cc6ba8d4058e
Tamima <tamimabw@gmail.com>
2024-08-15 15:35:21 +0600

    first commit

---
+++ Tamima/Data/TamimaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tamima.Models;
+
+namespace Tamima.Data
+{
+    public class TamimaContext : DbContext
+    {
+        public TamimaContext (DbContextOptions<TamimaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tamima.Models.User> User { get; set; } = default!;
+    }
+}
---
+++ Tamima/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tamima.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
